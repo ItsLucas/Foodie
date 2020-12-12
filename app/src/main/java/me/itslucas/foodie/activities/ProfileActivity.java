4be424602ddf7b048c653455a0596d4d741d7624
jonsonman <57242097+jonsonman@users.noreply.github.com>
2020-12-12 16:26:11 +0800

    ProfileActivity commit

---
+++ app/src/main/java/me/itslucas/foodie/activities/ProfileActivity.java
@@ -0,0 +1,13 @@
+package me.itslucas.foodie.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import me.itslucas.foodie.R;
+public class ProfileActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
