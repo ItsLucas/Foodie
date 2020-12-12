18cf3a82fed08179efa2b861ad8666a011fd37f2
jonsonman <57242097+jonsonman@users.noreply.github.com>
2020-12-12 16:13:51 +0800

    RegisterActivity commit

---
+++ app/src/main/java/me/itslucas/foodie/activities/RegisterActivity.java
@@ -0,0 +1,14 @@
+package me.itslucas.foodie.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import me.itslucas.foodie.R;
+public class RegisterActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
+
