c4c1fe97821ca0e2b55497fb835b8d4d11f662fe
jonsonman <57242097+jonsonman@users.noreply.github.com>
2020-12-12 15:51:29 +0800

    LoginActivity commit

---
+++ app/src/main/java/me/itslucas/foodie/activities/LoginActivity.java
@@ -0,0 +1,13 @@
+package me.itslucas.foodie.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import me.itslucas.foodie.R;
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
